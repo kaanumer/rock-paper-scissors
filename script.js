40b13216203d581151df13b0e27a8d1ae1ae9de0
kaanumer <118770833+kaanumer@users.noreply.github.com>
2025-07-27 18:36:45 +0000

    Add buttons to play the game.
    Add 3 (rock-paper-scissors buttons).
    Move CSS from internal to an external file.
    Update favicon.
    Add game logs and credits to HTML.

--- script.js
+++ script.js
@@ -12,44 +12,71 @@ function getComputerChoice() {
     };
 };
 
-function getHumanChoice() {
-    let humanChoice = prompt("choose rock, paper or scissors");
-    humanChoice = humanChoice.toLowerCase();
-    const validChoices = ["rock", "paper", "scissors"];
-    while (!validChoices.includes(humanChoice)) {
-        humanChoice = prompt(`invalid input. you can only choose rock, paper or scissors`);
-        humanChoice = humanChoice.toLowerCase();
-    };
-    return humanChoice;
-};
+function processClick(clickId) {
+    const gameScoreBoard = document.querySelector("#scoreboard");
+    let humanScoreText = document.querySelector(".human-score");
+    let computerScoreText = document.querySelector(".computer-score");
 
-function playRound(humanChoice, computerChoice) {
+    if (clickId === "reset") {
+        humanScore = 0;
+        humanScoreText.textContent = "0";
+        computerScore = 0;
+        computerScoreText.textContent = "0";
 
-    if (humanChoice === computerChoice) {
-        console.log(`draw! \tScoreboard -> H: ${humanScore} - C: ${computerScore}`);
+        const logs = document.querySelectorAll(".log")
+        logs.forEach((l) => l.remove());
+        
+    } else if (humanScore < 5 && computerScore < 5) {
+        let humanChoice = clickId;
+        let computerChoice = getComputerChoice();
+        let roundLog = document.createElement("p");
+        roundLog.classList.add("log");
+      
+        if (humanChoice === computerChoice) {
+            roundLog.textContent = `you picked ${humanChoice}, computer picked ${computerChoice}. draw!`;
+        }
+        else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")
+        ) {
+            humanScore++;
+            humanScoreText.textContent = humanScore;
+            roundLog.textContent = `you picked ${humanChoice}, computer picked ${computerChoice}. human wins!`
+        }
+        else {
+            computerScore++;
+            computerScoreText.textContent = computerScore;
+            roundLog.textContent = `you picked ${humanChoice}, computer picked ${computerChoice}. computer wins!`
+        };
+        
+        gameScoreBoard.appendChild(roundLog);
+
+        if (humanScore === 5) {
+            let finalGameLog = document.createElement("p");
+            finalGameLog.classList.add("log");
+            finalGameLog.textContent = "*** HUMAN WINS THE GAME ***";
+            gameScoreBoard.appendChild(finalGameLog);
+        } else if (computerScore === 5) {
+            let finalGameLog = document.createElement("p");
+            finalGameLog.classList.add("log");
+            finalGameLog.textContent = "*** COMPUTER WINS THE GAME***";
+            gameScoreBoard.appendChild(finalGameLog);
+        }
+    
+    } else {
+        // say game is already over
+        alert("Game ends at 5! Reset the game to play again.")
     }
-    else if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-        humanScore++;
-        console.log(`human wins! \tScoreboard -> H: ${humanScore} - C: ${computerScore}`);
-    }
-    else {
-        computerScore++;
-        console.log(`computer wins! \tScoreboard -> H: ${humanScore} - C: ${computerScore}`);
-    };
 };
 
 function playGame() {
-    for (let i = 1; i <= 5; i++) {
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        console.log(`Starting round ${i}. H: ${humanChoice}\tC: ${computerChoice}`);
-        playRound(humanChoice, computerChoice);
-    }
-    console.log(`---FINAL SCORES---\nH: ${humanScore} - C: ${computerScore}`)
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            processClick(button.id, humanScore, computerScore);
+        })
+    });
 };
 
 let humanScore = 0;
